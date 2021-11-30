d729055d3046407c8f059ec603bcd4f5f4cd4121
kr394 <95240589+kr394@users.noreply.github.com>
2021-11-30 11:10:40 -0700

    Update tetrisControl.h

--- lab7/tetrisControl.h
+++ lab7/tetrisControl.h
@@ -1,2 +1,14 @@
+#include <stdbool.h>
+#include <stdint.h>
 
-printf("H!");
+#ifndef TETRISCONTROL_H_
+#define TETRISCONTROL_H_
+
+// Used to init the state machine. Always provided though it may not be
+// necessary.
+void tetrisControl_init();
+
+// Standard tick function.
+void tetrisControl_tick();
+
+#endif /* TETRISCONTROL_H_ */
